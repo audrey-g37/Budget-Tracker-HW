4e721553d3908e42a383f39c73e15de98518c05d
Audrey <audrey.gillies@gmail.com>
2021-10-05 12:32:04 -0600

    created an indexedDB.js file to store the offline budget data.  updated server.js to use other port besides localhost.

---
+++ public/indexedDB.js
@@ -0,0 +1,65 @@
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+  db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.online) {
+    checkDb();
+  }
+};
+
+request.onerror = function (event) {
+  console.log(event.target.error);
+};
+
+function saveRecord(transaction) {
+  const transactionRecord = db.transaction(["pending"], "readwrite");
+  const budget = transactionRecord.objectStore("pending");
+  budget.add(transaction);
+}
+function checkDb() {
+  const transactionRecord = db.transaction(["pending"], "readwrite");
+  const store = transactionRecord.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.message) {
+            throw new Error(data);
+          }
+          // delete records if successful
+          const transactionRecord = db.transaction(["pending"], "readwrite");
+          const store = transactionRecord.objectStore("pending");
+          store.clear();
+          console.log("your transactions have been submitted");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+function deletePending() {
+  const transactionRecord = db.transaction(["pending"], "readwrite");
+  const store = transactionRecord.objectStore("pending");
+  store.clear();
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDb);
